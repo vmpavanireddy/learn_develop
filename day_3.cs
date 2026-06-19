c3f4116480d963684d6a4d82364b3a729e9341a2
vmpavanireddy <vmpavanireddy@gmail.com>
2026-06-19 21:33:29 +0530

    airthmetic operators

---
+++ day_3.cs
@@ -0,0 +1,28 @@
+//operators
+using System;   
+class day_3
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        //airthmetic operators
+        //are used to perform mathematical operations on operands of numeric data types
+        //they include addition (+), subtraction (-), multiplication (*), division (/), modulus (%)
+        int c = a + b; // addition
+        Console.WriteLine("Addition: " + c);
+        
+        int d = a - b; // subtraction
+        Console.WriteLine("Subtraction: " + d);
+        
+        int e = a * b; // multiplication
+        Console.WriteLine("Multiplication: " + e);
+        
+        int f = b / a; // division
+        Console.WriteLine("Division: " + f);
+        
+        int g = b % a; // modulus
+        Console.WriteLine("Modulus: " + g);
+
+        }
+}

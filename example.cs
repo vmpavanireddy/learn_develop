966d1cc75ac496c120eb13185f876c731dac011a
vmpavanireddy <vmpavanireddy@gmail.com>
2026-09-11 23:14:19 +0530

    Find Largest Element

--- example.cs
+++ example.cs
@@ -5,3 +5,17 @@ for (int i = 0; i < numbers.Length; i++)
 {
     Console.WriteLine(numbers[i]);
 }
+int[] num = { 25, 10, 45, 30, 15 };
+
+int largest = num[0];
+
+for (int i = 1; i < num.Length; i++)
+{
+    if (num[i] > largest)
+    {
+        largest = num[i];
+    }
+}
+
+Console.WriteLine("Largest: " + largest);
+
